38140a338b6b300a42db4915d5decd3eaa1e334c
Kaelan Patel <kaelanspatel@gmail.com>
2019-08-21 18:55:58 -0600

    initial commit

---
+++ game_functionality.py
@@ -0,0 +1,21 @@
+import math
+import matplotlib
+import tiles
+from random import choice
+from sqlalchemy import Column, Integer, Unicode, UnicodeText, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# engine = create_engine('sqlite:////tmp/teste.db', echo=True)
+# Base = declarative_base(bind=engine)
+
+class Country:
+    
+        
+def main():
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ tiles.py
@@ -0,0 +1,7 @@
+from random import choice
+
+terrains = ["plains", ""]
+
+class Tile:
+    
+    def __init__(self, terrain=choice()):
